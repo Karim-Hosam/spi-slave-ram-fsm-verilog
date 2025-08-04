3d5d3bd4660de108eae0c73bb8bab46feb9d06cd
Karim-Hosam <72357354+Karim-Hosam@users.noreply.github.com>
2025-08-04 15:30:38 +0300

    up Counter module + start in SPI Slave

--- src/Counter.v
+++ src/Counter.v
@@ -1,7 +1,18 @@
-module Counter(
-    input clk;
-    input reset,
+module up_counter #(
+    parameter COUNTER_BIT_SIZE = 4
+) (
+    input clk,
+    input clr,
     input enable,
+    output reg [COUNTER_BIT_SIZE-1:0] count
 );
-    
+
+  always @(posedge clk) begin
+    if (clr) begin
+      count <= 0;
+    end else if (enable) begin
+      count <= count + 1;  // Increment the counter
+    end
+  end
+
 endmodule
--- src/SPI_Slave.v
+++ src/SPI_Slave.v
@@ -1,5 +1,31 @@
-module SPI_Slave(
-    port_list
+module SPI_Slave (
+    input clk,
+    input rst_n,
+
+    input MOSI,
+    input SS_n,
+
+    input [7:0] tx_data,
+    input tx_valid,
+
+    output reg [9:0] rx_data,
+    output reg rx_valid,
+
+    output reg MISO
 );
-    
+
+  reg [3:0] bit_count;
+  reg clr_counter, enable_counter;
+
+  up_counter #(
+      .COUNTER_BIT_SIZE(4)
+  ) counter (
+      .clk(clk),
+      .clr(~rst_n),
+      .enable(rx_valid),
+      .count(rx_data)
+  );
+
+  
+
 endmodule
